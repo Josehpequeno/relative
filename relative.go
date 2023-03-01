d9b79767fdae1500bd46fe0b1d9ee6541c767e3a
Josehpequeno <hicarojbs21@gmail.com>
2023-02-28 21:15:56 -0300

    get command

--- relative.go
+++ relative.go
@@ -18,6 +18,7 @@ var app = tview.NewApplication()
 
 var flex = tview.NewFlex()
 var pages = tview.NewPages()
+var form = tview.NewForm()
 
 func main() {
 
@@ -37,9 +38,34 @@ func main() {
 		SetTextColor(secundaryColor).
 		SetText(folderLocation)
 
-	// _box := tview.NewBox().SetBorder(true).SetBorderColor(primaryColor).SetBackgroundColor(primaryColor).SetTitle(" Relative ").SetTitleColor(secundaryColor)
-	flex.SetDirection(tview.FlexRow).AddItem(text, 1, 1, false)
+	var cmdCommand = ""
+	form.AddInputField("command: ", "", 20, nil, func(input string) {
+		cmdCommand = input
+	}).SetFieldTextColor(secundaryColor).SetFieldBackgroundColor(primaryColor).SetBackgroundColor(primaryColor)
 
+	// _box := tview.NewBox().SetBorder(true).SetBorderColor(primaryColor).SetBackgroundColor(primaryColor).SetTitle(" Relative ").SetTitleColor(secundaryColor)
+	flex.SetDirection(tview.FlexRow).AddItem(text, 1, 1, false).AddItem(form, 0, 1, true)
+	flex.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Rune() == 113 {
+			app.Stop()
+		} else if event.Rune() == 13 {
+			cmd := exec.Command(cmdCommand)
+			cmd.Stdin = strings.NewReader("")
+			var out bytes.Buffer
+			cmd.Stdout = &out
+			err := cmd.Run()
+			if err != nil {
+				// log.Fatal(err)
+				log.Println("\n", err)
+			} else {
+				// form.Clear(true)
+				cmdCommand = ""
+				// fmt.Printf("\ncommand stdout %s: \n%s", cmdCommand, out.String())
+				fmt.Println("\n", out.String())
+			}
+		}
+		return event
+	})
 	pages.AddPage("Home", flex, true, true).SetBorder(true).SetBorderColor(primaryColor).SetBackgroundColor(primaryColor).SetTitle(" Relative ").SetTitleColor(secundaryColor)
 
 	if err := tview.NewApplication().SetRoot(pages, true).EnableMouse(true).Run(); err != nil {
