7702d19fb1e7a41bec6a8de138c9daa913efb58a
Josehpequeno <hicarojbs21@gmail.com>
2023-03-14 21:50:31 -0300

    bubble tea

--- relative.go
+++ relative.go
@@ -4,25 +4,49 @@ import (
 	"bytes"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"strings"
 
-	"github.com/gdamore/tcell/v2"
-	"github.com/rivo/tview"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
-var primaryColor = tcell.NewRGBColor(25, 36, 50)
-var secundaryColor = tcell.NewRGBColor(255, 250, 224)
+type model struct {
+	command       string
+	currentFolder string
+	commandOut    []string
+}
 
-var app = tview.NewApplication()
+func initialModel() model {
+	return model{
+		command:       "",
+		currentFolder: "/",
+		commandOut:    make([]string, 0),
+	}
+}
 
-var flex = tview.NewFlex()
-var pages = tview.NewPages()
-var form = tview.NewForm()
+func (m model) Init() tea.Cmd {
+	return nil
+}
 
-func main() {
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		case "enter", " ":
+			fmt.Println(m.command)
+			m.command = ""
+		default:
+			m.command += msg.String()
+		}
+	}
+	return m, nil
+}
 
-	tview.Styles.PrimitiveBackgroundColor = primaryColor
+func (m model) View() string {
+	s := "Relative"
 	cmd := exec.Command("pwd")
 	cmd.Stdin = strings.NewReader("")
 	var out bytes.Buffer
@@ -31,44 +55,16 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("command stdout: %q", out.String())
 	folderLocation := "Folder location current: " + out.String()
+	s += fmt.Sprintf("\n\n%s\n", folderLocation)
+	s += "\nPress q to quit.\n"
+	return s
+}
 
-	var text = tview.NewTextView().
-		SetTextColor(secundaryColor).
-		SetText(folderLocation)
-
-	var cmdCommand = ""
-	form.AddInputField("command: ", "", 20, nil, func(input string) {
-		cmdCommand = input
-	}).SetFieldTextColor(secundaryColor).SetFieldBackgroundColor(primaryColor).SetBackgroundColor(primaryColor)
-
-	// _box := tview.NewBox().SetBorder(true).SetBorderColor(primaryColor).SetBackgroundColor(primaryColor).SetTitle(" Relative ").SetTitleColor(secundaryColor)
-	flex.SetDirection(tview.FlexRow).AddItem(text, 1, 1, false).AddItem(form, 0, 1, true)
-	flex.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		if event.Rune() == 113 {
-			app.Stop()
-		} else if event.Rune() == 13 {
-			cmd := exec.Command(cmdCommand)
-			cmd.Stdin = strings.NewReader("")
-			var out bytes.Buffer
-			cmd.Stdout = &out
-			err := cmd.Run()
-			if err != nil {
-				// log.Fatal(err)
-				log.Println("\n", err)
-			} else {
-				// form.Clear(true)
-				cmdCommand = ""
-				// fmt.Printf("\ncommand stdout %s: \n%s", cmdCommand, out.String())
-				fmt.Println("\n", out.String())
-			}
-		}
-		return event
-	})
-	pages.AddPage("Home", flex, true, true).SetBorder(true).SetBorderColor(primaryColor).SetBackgroundColor(primaryColor).SetTitle(" Relative ").SetTitleColor(secundaryColor)
-
-	if err := tview.NewApplication().SetRoot(pages, true).EnableMouse(true).Run(); err != nil {
-		panic(err)
+func main() {
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
 	}
 }
